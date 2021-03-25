d0f69c4b6997f1955e80d033e497a3ee877f632d
jaguilar <jeison.a.r.2013@gmail.com>
2021-03-25 03:32:39 -0430

    se realizaron modificaciones en el componente Cou-Curr y su vista courses-curriculum para poder editar las secciones y añadir nuevas secciones---- aparte se creo el otro componente livewire CoursesLesson

---
+++ app/Http/Livewire/Instructor/CoursesLesson.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Instructor;
+
+use Livewire\Component;
+
+class CoursesLesson extends Component
+{
+    public function render()
+    {
+        return view('livewire.instructor.courses-lesson');
+    }
+}
--- resources/views/livewire/instructor/courses-curriculum.blade.php
+++ resources/views/livewire/instructor/courses-curriculum.blade.php
@@ -3,5 +3,78 @@
     <x-slot name="course">
         {{$course->slug}}
     </x-slot>
+
+    <h1 class="text-2xl font-bold">LECCIONES DEL CURSO</h1>
+    
+        <hr class="mt-2 mb-6">{{-- esta es el espacio de separación--}}
+
+        @foreach ($course->sections as $item)
+
+            <article class="card mb-6">
+                <div class="card-body bg-gray-200">
+
+                    @if ($section->id == $item->id)
+
+                        <form wire:submit.prevent="update">
+                            <input wire:model="section.name"  class="form-input w-full rounded" placeholder="Ingrese el nombre de la sección">
+                       
+                            @error('section.name')
+                                <span class="text-xs text-red-500">{{$message}}</span>                           
+                             @enderror
+                       
+                        </form>
+
+                    @else  
+                        <header class="flex justify-between items-center">
+                            <h1 class="cursor-pointer"><strong>Sección:</strong>{{$item->name}}</h1>
+
+                            <div>
+                                <i class="fas fa-edit cursor-pointer text-blue-500" wire:click="edit({{$item}})"></i>
+                                <i class="fas fa-eraser cursor-pointer text-red-500" wire:click="destroy({{$item}})"></i>
+                            </div>
+
+                        </header>
+
+                        <div>
+                            @livewire('instructor.courses-lesson', ['section' => $item], key($item->id))
+                        </div>
+
+                    @endif
+
+                </div>
+
+            </article>
+
+        @endforeach
+
+        <div x-data="{open: false}">
+            <a x-show="!open" x-on:click="open = true" class="flex items-center cursor-pointer mb-4 ml-2">
+                <i class="fas fa-plus-square text-2xl text-red-500 mr-2"></i>
+                 Agregar nueva sección
+            </a>
+
+            <article class="card" x-show="open">
+                <div class="card-body bg-gray-200">
+                    <h1 class="text-xl font-bold rounded mb-4"> Agregar nueva Sección </h1>
+
+                    <div class="mb-4">
+
+                        <input wire:model="name" class="form-input w-full" placeholder="Escriba el nombre de la sección">
+                        
+                        @error('name')
+                          <span class="text-xs text-red-500">{{$message}}</span>  
+                        @enderror
+                    
+                    </div>
+
+                    <div class="flex justify-end">
+                        <button class= "btn btn-danger" x-on:click="open = false">Cancelar</button>
+                        <button class="btn btn-primary ml-2" wire:click="store">Agregar</button>
+                    </div>
+
+                </div>
+
+            </article>
+        </div>
     
 </div>
