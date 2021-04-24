187829d49f29fbfdaad9ba3ac2cb09bd04975fa4
[jeison-git] <[jeison.a.r.2013@gmail.com]>
2021-04-24 01:39:22 -0430

    Esto soluciona el error anterior con la Api de Paypal.

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use function PHPUnit\Framework\once;
+
+use Illuminate\Http\Request;
+use App\Models\Course;
+
+class PaymentController extends Controller
+{
+    public function checkout(Course $course){
+        return view('payment.checkout', compact('course'));
+    }
+
+    public function pay(Course $course){
+
+        $apiContext = new \PayPal\Rest\ApiContext(
+            new \PayPal\Auth\OAuthTokenCredential(
+                config('services.paypal.client_id'),     // ClientID
+                config('services.paypal.client_secret')      // ClientSecret
+            )
+        );
+
+        $payer = new \PayPal\Api\Payer();
+        $payer->setPaymentMethod('paypal');
+
+        $amount = new \PayPal\Api\Amount();
+        $amount->setTotal($course->price->value);
+        $amount->setCurrency('USD');
+
+        $transaction = new \PayPal\Api\Transaction();
+        $transaction->setAmount($amount);
+
+        $redirectUrls = new \PayPal\Api\RedirectUrls();
+        $redirectUrls->setReturnUrl(route('payment.approved', $course))
+            ->setCancelUrl(route('payment.checkout', $course));
+
+        $payment = new \PayPal\Api\Payment();
+        $payment->setIntent('sale')
+            ->setPayer($payer)
+            ->setTransactions(array($transaction))
+            ->setRedirectUrls($redirectUrls);
+
+            try {
+                $payment->create($apiContext);
+
+                return redirect()->away($payment->getApprovalLink());                            
+            }
+            catch (\PayPal\Exception\PayPalConnectionException $ex) {
+                // This will print the detailed information on the exception.
+                //REALLY HELPFUL FOR DEBUGGING
+                echo $ex->getData();
+            }
+
+    }
+
+    public function approved(Request $request, Course $course){
+
+        $apiContext = new \PayPal\Rest\ApiContext(
+            new \PayPal\Auth\OAuthTokenCredential(
+                config('services.paypal.client_id'),     // ClientID
+                config('services.paypal.client_secret')      // ClientSecret
+            )
+        );
+
+        $paymentId = $_GET['paymentId'];
+        $payment = \PayPal\Api\Payment::get($paymentId, $apiContext);
+
+        $execution = new \PayPal\Api\PaymentExecution();
+        $execution->setPayerId($_GET['PayerID']);
+
+        $payment->execute($execution, $apiContext);
+         //Matricular al participante luego de efectuar el pago
+         $course->students()->attach(auth()->user()->id);
+
+         return redirect()->route('courses.status', $course);
+    }
+
+}
+
